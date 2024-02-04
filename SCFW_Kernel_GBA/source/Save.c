085b2e52e9c4b1c6b463ab67ba401b9f5455516f
Metroid Maniac <trmetroidmaniac@gmail.com>
2024-02-04 16:18:25 +0000

    Use tonccpy

--- SCFW_Kernel_GBA/source/Save.c
+++ SCFW_Kernel_GBA/source/Save.c
@@ -76,11 +76,64 @@ const struct save_type* save_findTag()
 	return NULL;
 }
 
+//tonccpy
+//
 void twoByteCpy(u16 *dst, const u16 *src, u32 size){
-	size >>= 1;
-	while(size--){
-		*dst = *src;
-		dst++;
-		src++;
+	if (size == 0 || dst == NULL || src == NULL)
+		return;
+
+	u32 count;
+	u16 *dst16;	 // hword destination
+	u8  *src8;	  // byte source
+
+	// Ideal case: copy by 4x words. Leaves tail for later.
+	if (((u32)src|(u32)dst) %4 == 0 && size >= 4) {
+		u32 *src32= (u32*)src, *dst32= (u32*)dst;
+
+		count = size/4;
+		u32 tmp = count&3;
+		count /= 4;
+
+		// Duff's Device, good friend!
+		switch(tmp) {
+			do {	*dst32++ = *src32++;
+		case 3:	 *dst32++ = *src32++;
+		case 2:	 *dst32++ = *src32++;
+		case 1:	 *dst32++ = *src32++;
+		case 0:	 ; } while (count--);
+		}
+
+		// Check for tail
+		size &= 3;
+		if (size == 0)
+			return;
+
+		src8 = (u8*)src32;
+		dst16 = (u16*)dst32;
+	} else {
+		// Unaligned
+
+		u32 dstOfs = (u32)dst&1;
+		src8 = (u8*)src;
+		dst16 = (u16*)(dst-dstOfs);
+
+		// Head: 1 byte.
+		if (dstOfs != 0) {
+			*dst16 = (*dst16 & 0xFF) | *src8++<<8;
+			dst16++;
+			if (--size == 0)
+				return;
+		}
 	}
+
+	// Unaligned main: copy by 2x byte.
+	count = size/2;
+	while (count--) {
+		*dst16++ = src8[0] | src8[1]<<8;
+		src8 += 2;
+	}
+
+	// Tail: 1 byte.
+	if (size & 1)
+		*dst16 = (*dst16 &~ 0xFF) | *src8;
 }
