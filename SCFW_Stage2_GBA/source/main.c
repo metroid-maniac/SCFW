4b3b749e09feec22cce715fa5245558c4aaac59c
Metroid Maniac <trmetroidmaniac@gmail.com>
2024-02-21 18:47:58 +0000

    Make firmware a little friendlier

--- SCFW_Stage2_GBA/source/main.c
+++ SCFW_Stage2_GBA/source/main.c
@@ -27,8 +27,16 @@ void sc_mode(u32 mode)
 }
 
 
-void waitForever() {
+void tryAgain() {
+	iprintf("Critical failure.\nPress A to restart.");
 	for (;;) {
+		scanKeys();
+		if (keysDown() & KEY_A)
+			for (;;) {
+				scanKeys();
+				if (keysUp() & KEY_A)
+					((void(*)()) 0x02000000)();
+			}
 		VBlankIntrWait();
 	}
 }
@@ -48,7 +56,7 @@ int main() {
 		iprintf("FAT system initialised\n");
 	} else {
 		iprintf("FAT initialisation failed!\n");
-		waitForever();
+		tryAgain();
 	}
 	
 	FILE *kernel = fopen("fat:/scfw/kernel.gba","rb");
@@ -56,16 +64,16 @@ int main() {
 		iprintf("Kernel file opened successfully\n");
 	} else {
 		iprintf("Kernel file open failed\n");
-		waitForever();
+		tryAgain();
 	}
 
 	fseek(kernel, 0, SEEK_END);
 	u32 kernel_size = ftell(kernel);
 	if (kernel_size > 0x40000) {
 		iprintf("Kernel too large to load!\n");
-		waitForever();
+		tryAgain();
 	}
-	iprintf("Kernel size is 0x%x bytes\n", kernel_size);
+	iprintf("Loading kernel\n\n");
 	fseek(kernel, 0, SEEK_SET);
 
 	u32 total_bytes = 0;
@@ -76,29 +84,29 @@ int main() {
 		for (u32 i = 0; i < bytes; i += 4) {
 			GBA_ROM[(i + total_bytes) >> 2] = *(vu32*) &filebuf[i];
 			if (GBA_ROM[(i + total_bytes) >> 2] != *(vu32*) &filebuf[i]) {
-				iprintf("SDRAM write failed at\n0x%x\n", i + total_bytes);
+				iprintf("\x1b[1A\x1b[KSDRAM write failed at\n0x%x\n\n", i + total_bytes);
 			}
 		}
 		sc_mode(SC_MEDIA);
 		total_bytes += bytes;
-		iprintf("Bytes read: 0x%x\n", total_bytes);
+		iprintf("\x1b[1A\x1b[K0x%x/0x%x\n", total_bytes, kernel_size);
 	} while (bytes);
 
 	if (ferror(kernel)) {
 		iprintf("Error reading kernel.\n");
-		waitForever();
+		tryAgain();
 	}
 
 	sc_mode(SC_RAM_RO);
 
 	if ((*GBA_ROM & 0xff000000) != 0xea000000) {
 		iprintf("Unexpected ROM entrypont, kernel not GBA ROM?");
-		waitForever();
+		tryAgain();
 	}
 
 	iprintf("Kernel loaded successfully.\n");
 	iprintf("Let's go.\n");
 	SoftReset(ROM_RESTART);
 	iprintf("Unreachable, panic!\n");
-	waitForever();
+	tryAgain();
 }
