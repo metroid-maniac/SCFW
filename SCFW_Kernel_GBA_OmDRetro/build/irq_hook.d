dced80e684196132bec858e1f0ff87c129976ec1
OmDRetro <12003408+OmDRetro@users.noreply.github.com>
2024-06-05 03:13:54 +0800

    File cleanup and push kernel revision kernel-20240605v0000 upstream

--- SCFW_Kernel_GBA_OmDRetro/build/irq_hook.d
+++
@@ -1,2 +0,0 @@
-irq_hook.o: \
- c:/Users/MSI-PAA-GEN4-PC/Documents/SuperCardSD_Stuff/Dev_Tools/Projects/SCFW-0.5.2/SCFW_Kernel_GBA/source/irq_hook.c
--- SCFW_Kernel_GBA_OmDRetro/build/my_io_scsd_s.d
+++
@@ -1,2 +0,0 @@
-my_io_scsd_s.o: \
- c:/Users/MSI-PAA-GEN4-PC/Documents/SuperCardSD_Stuff/Dev_Tools/Projects/SCFW-0.5.2/SCFW_Kernel_GBA/source/my_io_scsd_s.s
